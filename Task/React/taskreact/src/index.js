2ee995aeaa8cb940e84becfe8394756cbc9963ae
RishiTank47 <110224800+RishiTank47@users.noreply.github.com>
2023-06-08 14:29:21 +0530

    a

--- Task/React/taskreact/src/index.js
+++ Task/React/taskreact/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Fatch_API_in_Table from'./Task/Fatch_API_in_Table';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App />  */}
+    <Fatch_API_in_Table />
+
   </React.StrictMode>
 );
 
